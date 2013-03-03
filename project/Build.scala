03c28780209d506e3791df87a101f019c5f64026
Matthew Jaskula <mjaskula@gmail.com>
2013-03-03 00:07:14 -0500

    setting up mongo dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -12,13 +12,21 @@ object ApplicationBuild extends Build {
     jdbc,
     anorm,
     
+    "org.mongodb" %% "casbah" % "2.5.0",
+    "com.novus" %% "salat-core" % "1.9.2-SNAPSHOT",
+    "se.radley" %% "play-plugins-salat" % "1.2",
+    
     "org.testng" % "testng" % "6.8" % "test",
     "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    resolvers += "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases",
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId"
   )
 
 }
