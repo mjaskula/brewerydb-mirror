be86c6bc6a2cc8a4d57fc0de879992ac9d601579
Matthew Jaskula <mjaskula@gmail.com>
2013-03-03 01:03:58 -0500

    POC write beer json to mongo without parsing to scala

--- app/org/jaskula/brewerydbclient/BreweryDbClient.scala
+++ app/org/jaskula/brewerydbclient/BreweryDbClient.scala
@@ -3,14 +3,17 @@ package org.jaskula.brewerydbclient
 import play.api.libs.ws.WS
 import play.api.libs.json.Json
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.JsArray
+import play.api.libs.json.JsObject
+import scala.concurrent.Future
 
 class BreweryDbClient(apiKey: String) {
 
-  def beersForStyle(styleId: String) = {
+  def beersJsonForStyle(styleId: String): Future[Seq[JsObject]] = {
     WS.url("http://api.brewerydb.com/v2/beers")
       .withQueryString("key" -> apiKey,
                        "styleId" -> styleId).get().map { response =>
-      response.json \ "data"
+      (response.json \ "data").as[Seq[JsObject]]
     }
     
   }
--- app/org/jaskula/brewerydbmirror/Application.scala
+++ app/org/jaskula/brewerydbmirror/Application.scala
@@ -7,11 +7,16 @@ import org.jaskula.brewerydbclient.BreweryDbClient
 import play.Configuration
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.Play.current
+import com.mongodb.casbah.Imports._
+import play.api.libs.json.Json
+import com.mongodb.util.JSON
 
 object Application extends Controller {
   
   val config = play.api.Play.configuration
   val breweryDbClient = new BreweryDbClient(config.getString("brewerydb.apikey").getOrElse("specify apikey in 'conf/brewerydb.apikey.conf'"))
+  
+  val mongodb =  MongoClient()(config.getString("mongodb.default.db").getOrElse("test"))
   
   def index = Action {
     Ok("index")
@@ -23,8 +28,11 @@ object Application extends Controller {
 
   def populate() = Action {
   AsyncResult {
-    breweryDbClient.beersForStyle("25").map { beers =>
-      Ok(beers)
+    breweryDbClient.beersJsonForStyle("25").map { beers =>
+      beers.map { beer =>
+        mongodb("beers") += com.mongodb.util.JSON.parse(Json.stringify(beer)).asInstanceOf[DBObject]
+      }
+      Ok("crap!")
     }
   }  
 }
