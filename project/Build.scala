a9dce2aa809e39626b3e017da01136c47a7c6134
Matthew Jaskula <mjaskula@gmail.com>
2013-03-24 00:15:10 -0400

    removed unused lib salat

--- project/Build.scala
+++ project/Build.scala
@@ -16,21 +16,16 @@ object ApplicationBuild extends Build {
     "com.tzavellas" % "sse-guice" % "0.7.1",
     
     "org.mongodb" %% "casbah" % "2.5.0",
-    "com.novus" %% "salat-core" % "1.9.2-SNAPSHOT",
-    "se.radley" %% "play-plugins-salat" % "1.2",
     "joda-time" % "joda-time" % "2.1",
     
     "org.testng" % "testng" % "6.8" % "test",
     "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
   )
 
-
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
     resolvers += "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases",
-    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-    routesImport += "se.radley.plugin.salat.Binders._",
-    templatesImport += "org.bson.types.ObjectId"
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   )
 
 }
