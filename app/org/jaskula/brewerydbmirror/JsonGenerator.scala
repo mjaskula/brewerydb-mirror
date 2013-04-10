197605dc31369d002de3cdb50c6b7707450c1470
Matthew Jaskula <mjaskula@gmail.com>
2013-04-09 22:04:30 -0400

    remove the _id field from the generated json

--- app/org/jaskula/brewerydbmirror/JsonGenerator.scala
+++ app/org/jaskula/brewerydbmirror/JsonGenerator.scala
@@ -10,6 +10,9 @@ import play.api.libs.json._
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Writes._
+import play.api.libs.json.Reads._
 
 @Singleton
 class JsonGenerator {
@@ -18,13 +21,26 @@ class JsonGenerator {
   val db = ReactiveMongoPlugin.db
   lazy val collection = db("beers")
 
+  val beerTransformer = (JsPath \ '_id).json.prune
+
+  // TODO: is this overkill?
   def generateBeers(): Future[JsValue] = {
     val qb = QueryBuilder().query(Json.obj()).sort( "styleId" -> SortOrder.Ascending)
     
     // TODO: use Mongo Reader?
     collection.find[JsValue]( qb ).toList.map { persons =>
-      persons.foldLeft(JsArray(List()))( (obj, person) => obj ++ Json.arr(person) )
+      persons.foldLeft(JsArray(List())) { (obj, person) => 
+        person.transform(beerTransformer).fold(
+          invalid = { errors => 
+            play.Logger.error("Error validating json:" + errors.toString)
+            obj
+          },
+          valid = { res => 
+            obj ++ Json.arr(res)
+          }
+        )
+        
+      }
     }
   }
-  
 }
